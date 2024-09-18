dbf925b97555902d9fbcac2042db32dfe4db3e43
Hgaraboa7 <153545940+hgaraboa7@users.noreply.github.com>
2024-09-18 21:10:22 +0200

    Add files via upload
    Servicios1: ejecutar chrome.exe

---
+++ Java/Servicios-y-Procesos/Servicios1/src/main/java/com/servicios/servicios/Servicios.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.servicios.servicios;
+
+/**
+ *
+ * @author hector.garaboacasas
+ */
+public class Servicios {
+
+    public static void main(String[] args) {
+        String ruta="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+        ProcessBuilder pb;
+        try{
+            pb=new ProcessBuilder(ruta);
+            pb.start();
+        }catch(Exception e){
+            e.printStackTrace();}
+    }
+}
